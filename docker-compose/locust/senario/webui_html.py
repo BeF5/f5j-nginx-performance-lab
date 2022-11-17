1e700d2c4d1730cbb5c7f5096133518ea0ec42ba
Hiroshi Matsumoto <h.matsumoto@f5.com>
2022-11-17 12:16:01 +0900

    update

---
+++ docker-compose/locust/senario/webui_html.py
@@ -0,0 +1,10 @@
+import time
+from locust import HttpUser, task, between, constant_throughput, constant
+
+class QuickstartUser(HttpUser):
+    wait_time = constant_throughput(1)
+
+    @task
+    def hello_world(self):
+        self.client.get("/html/index.html")
+
